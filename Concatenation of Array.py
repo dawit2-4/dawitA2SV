495d4531995df23058a9d947afda72f7db54d229
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2024-05-18 14:05:59 +0300

    Update Concatenation of Array.py

--- Concatenation of Array.py
+++ Concatenation of Array.py
@@ -1,3 +1,16 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums+nums
+
+
+# or
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mylist = [0] * (2*n)
+
+        for i in range(n):
+            mylist[i] = nums[i]
+            mylist[i+n] = nums[i]
+        return mylist

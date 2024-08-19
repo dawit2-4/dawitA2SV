239f49dd50a19d76682aa5fc6b92f70764b12a2e
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2024-08-19 12:33:27 +0300

    Create Reverse Vowels of a String.py

---
+++ Reverse Vowels of a String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowel = {'a','e','i','o','u','A','E','I','O','U'}
+        l, r = 0, len(s) - 1
+        s = list(s)
+        while l < r:
+            if s[l] in vowel and s[r] in vowel:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+            elif s[l] in vowel:
+                r -= 1
+            elif s[r] in vowel:
+                l += 1
+            else:
+                l += 1
+                r -= 1
+        return ''.join(s)
+        

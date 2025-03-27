8bd879caeaf271bed0ce30bc65982b4b6fd344dc
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2025-03-27 16:57:18 +0300

    Time: 150 ms (81.05%), Space: 22.2 MB (54.9%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def validate(capacity):
+            cur_sum = 0
+            days_count = 1
+
+            for weight in weights:
+                cur_sum += weight
+
+                if cur_sum > capacity:
+                    cur_sum = weight
+                    days_count += 1
+                    if days_count > days:
+                        return False
+            return True
+                
+        high = sum(weights)
+        low = max(weights)
+
+        while low <= high:
+            mid = (high+low)//2
+
+            if validate(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low

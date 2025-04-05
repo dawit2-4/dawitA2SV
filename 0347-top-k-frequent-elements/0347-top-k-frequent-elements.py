7eecea8ae79ef4343ff50954261de104c16cf8e5
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2025-04-05 07:21:09 +0300

    Time: 11 ms (23.3%), Space: 24.4 MB (7.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,39 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ans = []
+        # count the frequency on numbers in num
         counts = Counter(nums)
-        arr = []
-        for value, count in counts.items():
-            arr.append([count, value])
-        
-        def partition(nums, left, right) :
-            random = randint(left,right)
-            nums[left], nums[random] = nums[random], nums[left]
-            pivot = left
+        bucket = [[] for _ in range(len(nums) + 1)]
 
-            write = left + 1
-            for read in range(left+1, right+1):
-                if nums[read] < nums[pivot]:
-                    nums[write], nums[read] = nums[read], nums[write]
-                    write += 1
-            nums[pivot], nums[write - 1] = nums[write-1], nums[pivot]
-            return write - 1
-        def quickSelect(nums, left, right):
-            if left == right:
-                return left
-            
-            pivot = partition(nums,left,right)
-            if pivot == len(nums)-k:
-                return pivot
-            elif pivot > len(nums) - k:
-                return quickSelect(nums, left, pivot - 1)
-            else:
-                return quickSelect(nums, pivot+ 1, right)
-        index = quickSelect(arr, 0, len(arr) - 1)
-        res = []
-        # print(arr)
-        for i in range(index, len(arr)):
-            res.append(arr[i][1])
-        return res
-            
+        #store the nums in freq based on their frequency
+        for value, count in counts.items():
+            bucket[count].append(value)
+        
+        n = len(bucket)
+        print(bucket)
+
+        for i in range(n-1, 0, -1):
+            for num in bucket[i]:
+                ans.append(num)
+                if len(ans) == k:
+                    return ans
+      
 
             

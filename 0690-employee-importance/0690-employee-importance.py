6230a45522d4458a8cf678fc2f229dfe6635c430
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2025-04-11 10:25:15 +0300

    Time: 131 ms (11.42%), Space: 18.8 MB (97.39%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,23 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        ans = 0
+       
+            
+        for i in range(len(employees)):
+            if employees[i].id == id:
+                importance = employees[i].importance
+                for subordinate in employees[i].subordinates:
+                    importance += self.getImportance(employees, subordinate)
+                return importance
+                
+
+            

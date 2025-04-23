219b577423595ce69067cd94ace7ffd95e03674a
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2025-04-23 08:52:20 +0300

    Time: 181 ms (39.49%), Space: 21.2 MB (31.11%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        def inbound(r, c):
+            return 0 <= r < len(mat) and 0 <= c < len(mat[0])
+        
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        queue = deque()
+        visited = [[False] * len(mat[0]) for _ in range(len(mat))]
+        res = [[0] * len(mat[0]) for _ in range(len(mat))]
+
+        # Step 1: Push all 0s to the queue and mark visited
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
+                    visited[r][c] = True
+
+        # Step 2: BFS from all 0s
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if inbound(nr, nc) and not visited[nr][nc]:
+                    res[nr][nc] = res[r][c] + 1
+                    visited[nr][nc] = True
+                    queue.append((nr, nc))
+
+        return res

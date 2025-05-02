6e26dacc4126bd94d875eb7144c12442ba24d086
dawit2-4 <150274289+dawit2-4@users.noreply.github.com>
2025-05-02 18:50:54 +0300

    Time: 3 ms (54.81%), Space: 18.2 MB (57.37%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -4,24 +4,24 @@ class Solution:
         max_n = 0
         for i, j in edges:
             max_n = max(max_n, max(i,j))
-        parent = [0] * (max_n+1)
-        
+        parent = [i for i in range(max_n +1)]
+        size = [1] * (max_n+1)
+        def find(n1):
+            if n1 == parent[n1]:
+                return n1
+            parent[n1] = find(parent[n1])
+            return parent[n1]
         for n1, n2 in edges:
-            
-            if parent[n1] == parent[n2] and parent[n1] != 0:
+            # print(parent)
+            root1 = find(n1)
+            root2 = find(n2)
+
+            if root1 == root2:
                 return [n1,n2]
-            elif parent[n1] == 0 and parent[n2] == 0:
-                parent[n1] = n2
-                parent[n2] = n2
             else:
-                if parent[n1] == 0:
-                    parent[n1] = parent[n2]
-                elif parent[n2] == 0:
-                    parent[n2] = parent[n1]
+                if size[root1] >= size[root2]:
+                    parent[root2] = root1
+                    size[root1] += size[root2]
                 else:
-                    prev = parent[n1]
-                    for i in range(len(parent)):
-                        if parent[i] == prev:
-                            parent[i] = parent[n2]
-                    
-        
+                    parent[root1] = root2
+                    size[root2] += size[root1]

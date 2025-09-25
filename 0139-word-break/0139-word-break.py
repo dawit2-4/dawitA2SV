06bcb259f2115a7b731c703595fd5a6467f66879
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-09-25 16:18:55 +0300

    Time: 4 ms (41.63%), Space: 18.1 MB (25.03%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,6 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        word_set = set(wordDict)     
+        word_set = wordDict     
         store = {}                   
 
         def dp(i):

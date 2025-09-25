a9c8ba5baa997b313d7a2af05929c89345479d4d
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-09-25 16:18:00 +0300

    Time: 0 ms (100%), Space: 18 MB (25.03%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)     
+        store = {}                   
+
+        def dp(i):
+            if i == len(s):          
+                return True
+            if i in store:           
+                return store[i]
+            
+            
+            for word in word_set:
+                if s.startswith(word, i):   
+                    if dp(i + len(word)):
+                        store[i] = True
+                        return True
+            store[i] = False
+            return False
+
+        return dp(0)

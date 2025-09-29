26e220b8722275f539c6693fa24477a97682763d
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-09-29 19:35:28 +0300

    Time: 0 ms (100%), Space: 17.5 MB (98.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,26 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        store = {}
-        # print(store)
-        def inbound(row, col):
-            return 0<= row < m and 0<= col < n
+        row =[1] * n
 
-        def dp(row,col):
-            if not inbound(row, col):
-                return 0
-            if (row, col) in store:
-                return store[(row, col)]
-            if row == m - 1 and col == n - 1:
-                return 1
-        
-            #down
-            down = dp(row, col+1)
-            #right
-            right = dp(row+1, col)
-
-            ans = right + down
-
-            store[(row, col)] = ans
-            return ans
-        return dp(0,0)
-        
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]

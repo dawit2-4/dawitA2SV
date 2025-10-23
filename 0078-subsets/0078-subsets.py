df02ab2ad29f60a9cc08180d5a6dbb6f2eaf6509
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-10-23 22:57:15 +0300

    Time: 0 ms (100%), Space: 17.8 MB (65.24%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        x = 2**n
         ans = []
-        for i in range(x):
-            temp = []
-            b = bin(i)[2:]
-            for k in range(n):
-                if (1 << k) & int(b) != 0:
-                    temp.append(nums[k])
-            ans.append(temp)
+
+        def backtrack(i, arr):
+            if i == len(nums):
+                ans.append(arr[:])
+                return
+
+            arr.append(nums[i])
+            backtrack(i+1, arr)
+            arr.pop()
+            backtrack(i+1, arr)
+        backtrack(0, [])
         return ans

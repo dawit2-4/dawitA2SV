473311af9d4944ff4d791dc87ffa8ea37c7a311d
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-10-28 17:06:55 +0300

    Time: 66 ms (51.37%), Space: 21.3 MB (32.37%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dist = [[float('inf')] * (len(word2) + 1) for _ in range(len(word1)+1)]
+        for i in range(len(word1)+1):
+            dist[i][len(word2)] = len(word1) - i
+        for j in range(len(word2)+1):
+            dist[len(word1)][j] = len(word2) - j
+        # print(dist)
+        for i in range(len(word1)-1, -1, -1):
+            for j in range(len(word2)-1, -1, -1):
+                if word1[i] == word2[j]:
+                    dist[i][j] = dist[i+1][j+1]
+                else:
+                    dist[i][j] = min(dist[i+1][j], dist[i][j+1], dist[i+1][j+1]) + 1
+        return dist[0][0]

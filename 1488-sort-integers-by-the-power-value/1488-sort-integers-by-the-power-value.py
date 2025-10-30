d9beca16576525c6df5250c75daf4f06cd5213f3
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-10-30 15:10:39 +0300

    Time: 531 ms (18.27%), Space: 17.8 MB (93.16%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.py
@@ -1,19 +1,17 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-from functools import cache
-
-@cache
-def get_pow(n):
-    if n == 1:
-        return 0
-
-    next_num = n//2 if n%2==0 else 3*n+1
-
-    return 1 + get_pow(next_num)
-
 class Solution:
+    def helper(self,num):
+        count = 0
+        while num !=1:
+            if num % 2 == 0:
+                num = num / 2
+            else:
+                num = (3 * num) + 1
+            count += 1
+        return count
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        sorted_nums = sorted(
-            range(lo, hi + 1),
-            key = get_pow
-        )
-        return sorted_nums[k-1]
+        ans = []
+        for i in range(lo, hi+1):
+            val = self.helper(i)
+            ans.append((val, i))
+        ans.sort()
+        return ans[k-1][1]

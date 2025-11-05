2168d43a942892da985f274b9e91d897a541d062
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-11-05 10:29:43 +0300

    Time: 163 ms (13.03%), Space: 18.3 MB (90.65%) - LeetHub

--- 1272-invalid-transactions/1272-invalid-transactions.py
+++ 1272-invalid-transactions/1272-invalid-transactions.py
@@ -4,24 +4,23 @@ class Solution:
 
         for t in transactions:
             name, time, amount, city = t.split(',')
-            parsed.append((name, int(time), int(amount), city, t))
+            parsed.append((name, int(time), int(amount), city))
         
         ans = set()
 
         for i in range(len(parsed)):
-            name, time, amount, city, t = parsed[i]
+            name, time, amount, city = parsed[i]
             if amount > 1000:
-                ans.add(t)
+                ans.add(i)
         for i in range(len(parsed)):
             for j in range(i+1, len(parsed)):
-                name1, time1, amount1, city1, t1 = parsed[i]
-                name2, time2, amount2, city2, t2 = parsed[j]
+                name1, time1, amount1, city1 = parsed[i]
+                name2, time2, amount2, city2= parsed[j]
                 if name1 == name2 and city1 != city2 and abs(time1 - time2) <= 60:
-                    ans.add(t1)
-                    ans.add(t2)
+                    ans.add(i)
+                    ans.add(j)
         result = []
-        for t in transactions:
-            if t in ans:
-                result.append(t)
+        for i in range(len(transactions)):
+            if i in ans:
+                result.append(transactions[i])
         return result
-                

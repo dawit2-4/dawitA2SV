7399d9ef29099e9301e36cb15099ac69525bba3b
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-11-05 17:28:45 +0300

    Time: 237 ms (43.58%), Space: 81.1 MB (5.79%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.py
@@ -1,16 +1,16 @@
 class Solution:
     @cache
-    def helper(self,num, count):
+    def helper(self,num):
         if num == 1:
-            return count
+            return 0
         if num % 2 == 0:
-            return self.helper(num /2, count +1)
+            return 1+self.helper(num /2)
         else:
-            return self.helper((3*num)+1, count+1)
+            return 1+self.helper((3*num)+1)
     def getKth(self, lo: int, hi: int, k: int) -> int:
         ans = []
         for i in range(lo, hi+1):
-            val = self.helper(i, 0)
+            val = self.helper(i)
             ans.append((val, i))
         ans.sort()
         return ans[k-1][1]

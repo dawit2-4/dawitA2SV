1bd0edddae58810376d3bca51f53888eb477c517
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-11-05 18:19:15 +0300

    Time: 8 ms (13.8%), Space: 19.4 MB (32.37%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,17 +1,19 @@
 class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        my_list = defaultdict(list)
-        for i in range(len(graph)):
-            my_list[i] = graph[i]
-        ans = []
-        def dfs(node, arr):
-            if node == len(graph) - 1:
-                arr.append(node)
-                ans.append(arr[:])
-                return 
-            for child in my_list[node]:
-                dfs(child, arr + [node])
-            return
-        dfs(0, [])
-        return ans
+    def allPathsSourceTarget(self, graphs: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list) # node -> [child1, child2 ....]
         
+        for i in range(len(graphs)):
+            graph[i] = graphs[i]
+        
+        paths = []
+        def dfs(node, path):
+            if node == len(graphs) - 1:
+                path.append(node)
+                paths.append(path[:])
+                return 
+            
+            for child in graph[node]:
+                dfs(child, path+[node])
+            return
+        dfs(0,[])
+        return paths

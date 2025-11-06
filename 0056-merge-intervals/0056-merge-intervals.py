68d8e29eb14ab9e3e76f1d32ab443cbf11564c50
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-11-06 12:08:15 +0300

    Time: 12 ms (15.98%), Space: 20.9 MB (90%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,7 +4,8 @@ class Solution:
         ans = [intervals[0]]
         for i in range(len(intervals)):
             if ans[-1][1] >= intervals[i][0]:
-                ans[-1][1] = max(intervals[i][1], ans[-1][1])
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
             else:
                 ans.append(intervals[i])
+        
         return ans

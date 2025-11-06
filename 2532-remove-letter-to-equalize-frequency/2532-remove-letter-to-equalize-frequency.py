e6f412c0cb318f4ae7a748e014b318f3ce6dbc3f
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2025-11-06 14:34:22 +0300

    Time: 0 ms (100%), Space: 17.8 MB (37.75%) - LeetHub

--- 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
@@ -1,14 +1,13 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
         count = Counter(word)
-        for ch in list(count.keys()):
-            count[ch] -= 1
-            if count[ch] == 0:
-                del count[ch]
-            s = set(count.values())
-            if len(s) == 1:
+
+        for key in list(count.keys()):
+            count[key] -= 1
+            if count[key] == 0:
+                del count[key]
+            if len(set(count.values())) == 1:
                 return True
-            count[ch] += 1
+            count[key] += 1
         return False
-            
-            
+        

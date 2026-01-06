f50811e98a751627ad8245b169d3118f8029f186
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2026-01-06 08:07:48 +0300

    Time: 5862 ms (13.39%), Space: 19.6 MB (15.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,36 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows = len(board)
-        cols = len(board[0])
-        def in_bound(r,c):
-            return 0 <= r < rows and 0 <= c < cols
-        dxn = ((0,1), (1, 0),  (0, -1), (-1, 0))
-        
-        visited = set()
-        def dfs(r, c, i):
-            if board[r][c] != word[i]:
-                return False
-            if i == len(word) - 1:
-                return True
-            visited.add((r,c))
-            for row, col in dxn:
-                nr, nc = row + r, col + c
-                if in_bound(nr,nc) and (nr, nc) not in visited:
-                    if dfs(nr, nc, i+1):
-                        return True
-            visited.remove((r,c))
-            return False
-        ans = False
-        for i in range(rows):
-            for j in range(cols):
-                    ans |= dfs(i,j,0)
-        return ans
+        def helper(i, j, visited, idx):
             
+            if (i, j) in visited:
+                return False
+            if board[i][j] != word[idx]:
+                return False
+            if idx == len(word) - 1:
+                return True
+            visited.add((i, j))
+            
+            for dr, dc in direction:
+                new_row, new_col = i + dr, j + dc
+                if inbound(new_row, new_col):
+                    response = helper(new_row, new_col, visited, idx + 1)
+                    if response:
+                        return True
+            visited.remove((i,j))
+            return False
+
+        direction = ((1,0), (0,1), (-1,0), (0,-1))
+        def inbound(r, c):
+            return 0 <= r < len(board) and 0 <= c < len(board[0])
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    if helper(i,j,set(), 0):
+                        return True
+        return False
+        
+        
+
+
+            
+

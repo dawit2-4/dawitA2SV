accdb517be4aeef7cfefe75973b772e5922ca8a4
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2026-01-10 21:00:53 +0300

    Time: 292 ms (12.88%), Space: 31.5 MB (5.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        direction = ((0, 1), (0, -1), (1, 0), (-1, 0))
-        def inbound(r, c):
-            return 0 <= r < rows and 0 <= c < cols
+
+        def inbound(r,c):
+            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+        ans = 0
+
+        direction = [(-1,0), (1, 0), (0, -1), (0, 1)]
         visited = set()
         def dfs(r, c):
-            if not inbound(r,c):
-                return 
-            if (r,c) in visited:
-                return
-            visited.add((r,c))
-           
+            visited.add((r, c))
             
-            
-            for nr, nc in direction:
-                nnr = r + nr
-                nnc = c + nc
+            for dx, dy in direction:
+                new_r, new_c = r + dx, c + dy
+                if (new_r, new_c) not in visited and inbound(new_r, new_c) and grid[new_r][new_c] == "1":
+                    dfs(new_r, new_c)
+            return 
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if (r, c) not in visited and grid[r][c] == "1":
+                    ans += 1
+                    dfs(r, c)
 
-                if inbound(nnr, nnc) and (nnr, nnc) not in visited and grid[nnr][nnc] == '1':
-                    dfs(nnr, nnc)
-            return
-        count = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1" and (i, j) not in visited:
-                    count += 1
-                    dfs(i,j)
-        return count
+                    
+        return ans

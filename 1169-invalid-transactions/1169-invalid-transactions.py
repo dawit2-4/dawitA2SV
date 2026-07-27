49e2d10b33cdbde85beb9db06cca9e1fae97a8ba
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2026-07-27 17:41:07 +0300

    Time: 30 ms (54.54%), Space: 20.2 MB (7.14%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,38 +1,40 @@
-# import csv
-
-# def extract_transactions(f_name):
-#     with open(f_name) as f:
-#         reader = csv.DictReader(f)
-#         transactions = list(reader)
-#     return transactions
-
-
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        store_transaction = defaultdict(list)
-        fraud = []
-        invalid = [False] * len(transactions)
+        store = defaultdict(list)
+        invalid = []
+        transaction = []
         for i in range(len(transactions)):
-            name, time, amount, city = transactions[i].split(",")
+            transaction.append(transactions[i].split(","))
+            transaction[i].append(i)
+           
+            
+        for t in transaction:
+            _, _, amount, _, idx = t
             if int(amount) > 1000:
-                invalid[i] = True
-            store_transaction[name].append((time, amount,city, i))
-    
-        for name in store_transaction:
-            store_transaction[name].sort()
-            txs = store_transaction[name]
-            for i in range(len(txs)):
-                for j in range(1, len(txs)):
-                    time1, amount1, city1, idx1 = txs[i]
-                    time2, amount2, city2, idx2 = txs[j]
+                invalid.append(idx)
+        
 
-                    if city1 != city2 and abs(int(time1)  - int(time2)) <= 60:
-                        invalid[idx1] = True
-                        invalid[idx2] = True
-        for i in range(len(transactions)):
-            if invalid[i]:
-                fraud.append(transactions[i])
+        for t in transaction:
+            name, time, amount, city, idx = t
+            store[name].append((int(time), amount, city, idx))
+        
+        for key, val in store.items():
+            name = key
+            val.sort()
 
-        return fraud
-                    
-                    
+            if len(val) > 1:
+                for i in range(len(val)):
+                    for j in range(i+1, len(val)):
+                        if val[i][2] != val[j][2] and val[j][0] - val[i][0] <= 60:
+                            idx1 = val[i][3]
+                            idx2 = val[j][3]
+                            invalid.append(idx1)
+                            invalid.append(idx2)
+        
+
+                            
+        invalid = set(invalid)
+        ans = []
+        for i in invalid:
+            ans.append(transactions[i])
+        return ans

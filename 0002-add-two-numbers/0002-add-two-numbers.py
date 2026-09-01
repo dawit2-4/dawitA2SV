f8365717f4d84d660d99ca93004c79bd673e9821
Dawit Birhanu <150274289+dawit2-4@users.noreply.github.com>
2026-09-01 11:39:50 +0300

    Time: 2 ms (70.17%), Space: 19.3 MB (41.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,27 +5,46 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        node1 = l1
-        node2 = l2
-        ans = ListNode()
+        ans = ListNode(0)
         ptr = ans
         carry = 0
-        while node1 or node2 or carry:
-            v1 = node1.val if node1 else 0
-            v2 = node2.val if node2 else 0
-            summ = v1 + v2 + carry
-            carry = summ // 10
-            val = summ % 10
-            
-            ptr.next = ListNode(val)
-            ptr = ptr.next
 
-            if node1:
-                node1 = node1.next
-            if node2: 
-                node2 = node2.next
+        while l1 and l2:
+            summ = l1.val + l2.val + carry
+            if summ >= 10:
+                reminder = summ % 10
+                ptr.next = ListNode(reminder)
+                carry = 1
+            else:
+                carry = 0
+                ptr.next = ListNode(summ) 
+            
+            ptr = ptr.next
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            summ = l1.val + carry
+            if summ >= 10:
+                reminder = summ % 10
+                ptr.next = ListNode(reminder)
+                carry = 1
+            else:
+                carry = 0
+                ptr.next = ListNode(summ)
+            l1 = l1.next
+            ptr = ptr.next
+        while l2:
+            summ = l2.val + carry
+            if summ >= 10:
+                reminder = summ % 10
+                ptr.next = ListNode(reminder)
+                carry = 1
+            else:
+                carry = 0
+                ptr.next = ListNode(summ)
+            l2 = l2.next
+            ptr = ptr.next
+        if carry == 1:
+            ptr.next = ListNode(1)
         return ans.next
 
-        
-        carry = 0
-        
